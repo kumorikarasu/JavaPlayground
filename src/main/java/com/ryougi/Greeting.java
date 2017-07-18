1603219a7e378cbb1dfc961d28f3fee454a9a238
Kumori <kumori@ryougi.com>
2017-07-18 11:28:42 -0400

    Added test class

---
+++ src/main/java/com/ryougi/Greeting.java
@@ -0,0 +1,15 @@
+package com.ryougi;
+
+/**
+ * Created by Sean on 7/18/2017.
+ */
+public class Greeting {
+    String in;
+    public Greeting(String input){
+        in = input;
+    }
+
+    public String toString(){
+        return in;
+    }
+}
--- src/main/java/com/ryougi/first.java
+++ src/main/java/com/ryougi/first.java
@@ -3,7 +3,8 @@ package com.ryougi;
 /**
  * Created by Sean on 7/17/2017.
  */
-public class first {
+public class First {
+
     public static void main (String[] args) {
 
         System.out.println("Hello World");
