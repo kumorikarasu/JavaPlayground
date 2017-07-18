8d4401fb45dc56057b326e9fc7005cf6cb35bfbd
Kumori <kumori@ryougi.com>
2017-07-18 11:03:00 -0400

    pom file

---
+++ src/main/java/com/ryougi/first.java
@@ -0,0 +1,12 @@
+package com.ryougi;
+
+/**
+ * Created by Sean on 7/17/2017.
+ */
+public class first {
+    public static void main (String[] args) {
+
+        System.out.println("Hello World");
+
+    }
+}
